8ef8b673c6b6c2884086941be216315436ec049f
Travis Wise <86330962+TravisMWise@users.noreply.github.com>
2022-07-22 23:10:00 -0400

    Finished chapter 4

--- Chatper4/drawings.js
+++ Chatper4/drawings.js
@@ -46,6 +46,90 @@ function draw_pacman(ctx, x, y, radius, value) {
     ctx.fill();
 }
 
-function draw_ship(ctx, x, y, radius, options) {
+function draw_ship(ctx, radius, options) {
+    ctx.save();
+    const PI = Math.PI;
+    // Set default values
+    options = options || {};
+    ctx.lineWidth = options.lineWidth || 2;
+    ctx.strokeStyle = options.stroke || "white";
+    ctx.fillStyle = options.fill || "black";
+    let angle = (options.angle || (1/2) * PI) / 2;
+    let curve1 = options.curve1 || 0.25;
+    let curve2 = options.curve2 || 0.75;
 
+    // Optionally draw a guide showing the collision radius
+    if (options.guide) {
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "rgba(0,0,0,0.5)";
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.arc(0,0,radius,0,2*PI);
+        ctx.stroke();
+        ctx.fill();
+    }
+
+    // Draw the ship in three lines
+    ctx.beginPath();
+    ctx.moveTo(radius, 0); // Start at the right point
+    ctx.quadraticCurveTo(
+        Math.cos(angle) * radius * curve2,
+        Math.sin(angle) * radius * curve2,
+        Math.cos(Math.PI - angle) * radius,
+        Math.sin(Math.PI - angle) * radius
+    );
+    ctx.quadraticCurveTo(
+        -radius * curve1, 
+        0,
+        Math.cos(Math.PI + angle) * radius,
+        Math.sin(Math.PI + angle) * radius
+    );
+    ctx.quadraticCurveTo(
+        Math.cos(-angle) * radius * curve2,
+        Math.sin(-angle) * radius * curve2,
+        radius, 
+        0
+    );
+    ctx.closePath();
+    ctx.fill();
+    ctx.stroke();
+
+    // Guide lines and control points
+    if(options.guide) {
+        ctx.strokeStyle = "white";
+        ctx.fillStyle = "white";
+        ctx.lineWidth = 0.5;
+        ctx.beginPath();
+        ctx.moveTo(
+            Math.cos(-angle) * radius,
+            Math.sin(-angle) * radius
+        );
+        ctx.lineTo(0, 0);
+        ctx.lineTo(
+            Math.cos(angle) * radius,
+            Math.sin(angle) * radius
+        );
+        ctx.moveTo(-radius, 0);
+        ctx.lineTo(0, 0);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(
+            Math.cos(angle) * radius * curve2,
+            Math.sin(angle) * radius * curve2,
+            radius/40, 0, 2 * Math.PI
+        );
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(
+            Math.cos(-angle) * radius * curve2,
+            Math.sin(-angle) * radius * curve2,
+            radius/40, 0, 2 * Math.PI
+        );
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(radius * curve1 - radius, 0, radius/50, 0, 2 *
+        Math.PI);
+        ctx.fill();
+    }
+    ctx.restore();
 }
